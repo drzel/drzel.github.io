fb177940344edfb560493b9134fcf09e14e23d44
drzel <shayolden@hotail.com>
2015-01-31 18:58:56 +0900

    add gcal support

--- js/schedule.js
+++ js/schedule.js
@@ -2,5 +2,35 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
     $('#calendar').fullCalendar({
     // put your options and callbacks here
+    	eventRender: function(event, element){
+	    	if (event.title == "GABA Lesson - NA"){
+	    		return false;
+	    	}
+	    },
+    	header: {
+    		left: 'prev',
+    		center: 'today month,agendaWeek',	
+    		right: 'next',
+    	},
+    	defaultView: 'agendaWeek',
+    	slotDuration: '01:00:00',
+    	weekends: false,
+    	minTime: "10:00:00",
+    	maxTime: "22:00:00",
+    	allDaySlot: false,
+    	contentHeight: "auto",
+    	eventColor: "hsl(220,20%,30%)",
+    	googleCalendarApiKey: 'AIzaSyBdqGj_F2h02H5MV6PqjfuTzpee2KEJYQM',
+    	eventSources: [
+            {
+                googleCalendarId: '2t9c26ch825qeeukm0guu74c44@group.calendar.google.com',
+                className: 'eoc-calendar',
+            },
+            {
+                googleCalendarId: '04ubl8ju5lr7osddqp49pveqd3glfioa@import.calendar.google.com',
+                className: 'gaba-calendar',
+                color: 'hsl(120,20%,30%)',
+            }
+    	]
     })
 });
